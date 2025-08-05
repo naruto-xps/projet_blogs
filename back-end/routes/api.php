fdcc8ff5e1a4ae8c2f38946de484afdaa77d54bf
naruto-xps <nu320617@gmail.com>
2025-08-05 12:56:02 +0200

    reglage docker

--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -7,16 +7,6 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FriendController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 // Routes d'authentification
 Route::post('/auth/register', [AuthController::class, 'register']);
